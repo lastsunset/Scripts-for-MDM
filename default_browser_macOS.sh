b1666fd459e5b7327b32fb27083180660509570f
a.nazarev <alexnaznvkz@gmail.com>
2026-06-11 12:40:40 +0200

    Fix path for LaunchServices plist file

--- default_browser_macOS.sh
+++ default_browser_macOS.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-x=~/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist; \
+x=/Users/*/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist; \
 plutil -convert xml1 $x; \
 grep 'https' -b3 $x | awk 'NR==2 {split($2, arr, "[><]"); print arr[3]}'; \
 plutil -convert binary1 $x
