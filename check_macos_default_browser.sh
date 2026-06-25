2905b736dc176a91e523c3299a3987fe9b4161ba
a.nazarev <alexnaznvkz@gmail.com>
2026-06-25 11:37:25 +0200

    Add script to check macOS default browser

---
+++ check_macos_default_browser.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+x=/Users/*/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist; \
+plutil -convert xml1 $x; \
+grep 'https' -b3 $x | awk 'NR==2 {split($2, arr, "[><]"); print arr[3]}'; \
+plutil -convert binary1 $x
